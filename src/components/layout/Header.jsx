97bd959dd6ad33648c6e6a3d46b80d1a20a800cd
Darius <dariuszdroba@gmail.com>
2024-02-27 17:29:29 +0200

    Destination component hierarchy

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="pt-3 pl-2">
       <img src={logo} alt="" style={{ height: "35px", verticalAlign: "top" }} />
-      <span className="h2 pt-4 text-white-50">travelOpedia</span>
+      <span className="offset-5 h2 pt-4 text-white-50">travelOpedia</span>
     </div>
   );
 };
